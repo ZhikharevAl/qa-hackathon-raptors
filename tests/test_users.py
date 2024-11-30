063541868338798f6a080343eb2d123c0bcfd6c6
AlexeyZh <waltafunk@gmail.com>
2024-11-30 23:50:50 +0300

    test: added test_get_user

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,9 @@
-import pytest
+from typing import Any
 
-from services.users.models.user_models import UsersResponse
+import pytest
+from pydantic_core import ValidationError
+
+from services.users.models.user_models import UserResponse, UsersResponse
 from services.users.user_api_client import UserAPIClient
 from utils.helpers import UserAPITestHelpers
 
@@ -18,10 +21,10 @@ class TestUsersAPI:
         """
         response_data = user_api_client.get_users(task_id, offset=0, limit=10)
 
-        from pydantic import ValidationError
-
         try:
-            validated_response = UsersResponse.model_validate(response_data)
+            validated_response: UsersResponse = UsersResponse.model_validate(
+                response_data
+            )
         except ValidationError as e:
             msg = f"Response validation failed for Task-Id {task_id}: {e}"
             raise AssertionError(msg) from e
@@ -35,3 +38,24 @@ class TestUsersAPI:
         ), f"Users list should not be empty for Task-Id {task_id}"
 
         UserAPITestHelpers.validate_offset(user_api_client, task_id, validated_response)
+
+    def test_get_user(self, user_api_client: UserAPIClient) -> None:
+        """
+        Test getting user details by UUID.
+
+        :param user_api_client: Instance of UserAPIClient
+        """
+        task_id = "api-23"
+        user_uuid = "23403afa-483c-4803-8af1-2d1316f1460f"
+        user_data: dict[str, Any] = user_api_client.get_user(user_uuid, task_id)
+        expected_keys = ["avatar_url", "email", "name", "nickname", "uuid"]
+        assert len(user_data) == len(
+            expected_keys
+        ), f"Expected {len(expected_keys)} keys, but got {len(user_data)}"
+        assert user_data["uuid"] == user_uuid, "UUIDs don't match"
+
+        try:
+            UserResponse.model_validate(user_data)
+        except ValidationError as e:
+            msg = f"User data validation failed: {e}"
+            raise AssertionError(msg) from e
