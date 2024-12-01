81f0b05bd6d656df98cf85dd70f084c99364a250
AlexeyZh <waltafunk@gmail.com>
2024-12-02 01:13:25 +0300

    fix: query

--- services/games/games_api_client.py
+++ services/games/games_api_client.py
@@ -1,3 +1,4 @@
+import urllib.parse
 from typing import Any
 
 from playwright.sync_api import APIRequestContext
@@ -19,17 +20,30 @@ class GamesAPIClient(HTTPClient):
         """
         super().__init__(api_context)
 
-    def get_games(
-        self, task_id: str, offset: int = 0, limit: int = 10
+    def search_games(
+        self, task_id: str, query: str = "", offset: int = 0, limit: int = 10
     ) -> dict[str, Any]:
         """
-        Getting a list of games with a Task-Id.
+        Getting a list of games with a Task-Id and optional search query.
 
         :param task_id: Task ID
+        :param query: Search query for game names
+        :param offset: Offset for pagination
+        :param limit: Limit of results per page
         :return: Response from the API
         """
         headers = {"X-Task-Id": task_id}
-        endpoint = f"{GAMES_ENDPOINT}?offset={offset}&limit={limit}"
+
+        encoded_query = urllib.parse.quote(query)
+
+        if query:
+            endpoint = (
+                f"{GAMES_ENDPOINT}/search/"
+                f"?query={encoded_query}&offset={offset}&limit={limit}"
+            )
+        else:
+            endpoint = f"{GAMES_ENDPOINT}/search/" f"?offset={offset}&limit={limit}"
+
         response = self.get(endpoint, headers=headers)
 
         if not response.ok:
