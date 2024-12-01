c69a880215352137c8d7d52aa734b60306a9fb6a
AlexeyZh <waltafunk@gmail.com>
2024-12-01 22:28:04 +0300

    feat: added methods

--- services/http_client.py
+++ services/http_client.py
@@ -26,3 +26,67 @@ class HTTPClient:
         )
         AllureUtils.attach_response(response)
         return response
+
+    def delete(
+        self,
+        endpoint: str,
+        headers: dict[str, Any] | None = None,
+        params: dict[str, Any] | None = None,
+    ) -> APIResponse:
+        """Sends a DELETE request to the specified endpoint with the provided data."""
+        response: APIResponse = self.api_request_context.delete(
+            endpoint, headers=headers, params=params, timeout=TIMEOUT
+        )
+        AllureUtils.attach_response(response)
+        return response
+
+    def post(
+        self,
+        endpoint: str,
+        headers: dict[str, Any] | None = None,
+        json: dict[str, Any] | None = None,
+        data: dict[str, Any] | None = None,
+    ) -> APIResponse:
+        """Sends a POST request to the specified endpoint with the provided data."""
+        kwargs = {}
+        if headers:
+            kwargs["headers"] = headers
+
+        if json is not None:
+            kwargs["data"] = json
+        elif data is not None:
+            kwargs["data"] = data
+
+        response: APIResponse = self.api_request_context.post(
+            endpoint, **kwargs, timeout=TIMEOUT
+        )
+        AllureUtils.attach_response(response)
+        return response
+
+    def put(
+        self,
+        endpoint: str,
+        headers: dict[str, Any] | None = None,
+        json: dict[str, Any] | None = None,
+        data: dict[str, Any] | None = None,
+    ) -> APIResponse:
+        """Sends a PUT request to the specified endpoint with the provided data."""
+        response: APIResponse = self.api_request_context.put(
+            endpoint, headers=headers, json=json, data=data, timeout=TIMEOUT
+        )
+        AllureUtils.attach_response(response)
+        return response
+
+    def patch(
+        self,
+        endpoint: str,
+        headers: dict[str, Any] | None = None,
+        json: dict[str, Any] | None = None,
+        data: dict[str, Any] | None = None,
+    ) -> APIResponse:
+        """Sends a PATCH request to the specified endpoint with the provided data."""
+        response: APIResponse = self.api_request_context.patch(
+            endpoint, headers=headers, json=json, data=data, timeout=TIMEOUT
+        )
+        AllureUtils.attach_response(response)
+        return response
