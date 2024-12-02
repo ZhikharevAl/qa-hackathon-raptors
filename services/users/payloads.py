3a119fbfc7d36a7b7720f036debe1ab37febc944
AlexeyZh <waltafunk@gmail.com>
2024-12-02 04:27:01 +0300

    feat: added update user

--- services/users/payloads.py
+++ services/users/payloads.py
@@ -14,3 +14,10 @@ class Payloads:
         "name": fake.first_name(),
         "nickname": fake.user_name(),
     }
+
+    update_user: ClassVar[dict[str, str]] = {
+        "name": fake.first_name(),
+        "nickname": fake.user_name(),
+        "email": fake.email(),
+        "password": fake.password(length=10),
+    }
